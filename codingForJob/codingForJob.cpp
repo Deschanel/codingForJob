d21f427f53c9e1bdd4231dd049ede5f4d4eebe52
Deschanel <201400150159@mail.sdu.edu.cn>
2019-10-25 22:11:52 +0800

    做了四个题

--- codingForJob/codingForJob.cpp
+++ codingForJob/codingForJob.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <map>
 #include <stack>
+#include <string>
 
 using namespace std;
 
@@ -78,65 +79,229 @@ vector<int> twoSum_3(vector<int> &nums, int target)  //一个map表, 改进二
 	return result;
 }
 
-int reverseNum_1(int x)  //这个方法慢
+int reverseNum(int x)  //这个方法
 {
 	int result = 0, minNum = -pow(2, 31), maxNum = pow(2, 31) - 1;
-	stack<int> s;
-	if (x > 0)
+	while (x != 0)
 	{
-		while (x > 0)
+		int tmp = x % 10;
+		if (result > maxNum / 10 || (result == maxNum/10 && tmp > 7))
 		{
-			int tmp = x % 10;
-			s.push(tmp);
-			x /= 10;
+			return 0;
 		}
-		int i = 0;
-		while (!s.empty())
+		else if(result < minNum / 10 || (result == minNum/10 && tmp < -8))
 		{
-			result += (s.top()*pow(10, i));
-			if (result < minNum || result > maxNum)
-			{
-				return 0;
-			}
-			s.pop();
-			i++;
+			return 0;
 		}
-	}
-	else
-	{
-		x = abs(x);
-		while (x > 0)
-		{
-			int tmp = x % 10;
-			s.push(tmp);
-			x /= 10;
-		}
-		int i = 0;
-		while (!s.empty())
-		{
-			result += (s.top()*pow(10, i));
-			if (result < minNum || result > maxNum)
-			{
-				return 0;
-			}
-			s.pop();
-			i++;
-		}
-		result *= -1;
+		result = result * 10 + tmp;
+		x /= 10;
 	}
 	return result;
 }
 
-int reverseNum_2(int x)  //改进
+bool isPalindrome_1(int x)  //方法一反转数字看是否相等
 {
-	int result = 0, minNum = -pow(2, 31), maxNum = pow(2, 31) - 1;
+	if (x < 0)
+	{
+		return false;
+	}
+	if (x == 0)
+	{
+		return true;
+	}
+	unsigned int i = 1, result = 0, xt = x;
 	while (x > 0)
 	{
 		int tmp = x % 10;
-		int tmp_1 = x / 10;
-		result += tmp;
-		x = tmp_1;
+		//这里应加上判断if (result > maxNum / 10 || (result == maxNum/10 && tmp > 7))
+		result = result * 10 + tmp;
+		x /= 10;
 	}
+	if (result == xt)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int isPalindrome_2(int x)  //改进，由于x肯定不会溢出，那么其对称中心的左右两边数字肯定也不会溢出,因此只需要判断对称中心两边是否相等
+{
+	if (x == 0)
+	{
+		return true;
+	}
+	if (x < 0 || x % 10 == 0)
+	{
+		return false;
+	}
+	int left = x, right = 0;
+	while (left > right)  //若left不大于right，则说明已经在或者超过了对称中心，因此结束循环
+	{
+		int tmp = left % 10;
+		right = right * 10 + tmp;
+		left /= 10;
+	}
+	return (left == right) || (left == right / 10);
+}
+
+int romanToInt_1(string s)
+{
+	stack<int> st;
+	int result = 0;
+	for (int i=0; i<s.size(); ++i)
+	{
+		switch (s.at(i))
+		{
+		case 'I':
+			st.push(1);
+			break;
+		case 'V':
+			st.push(5);
+			break;
+		case 'X':
+			st.push(10);
+			break;
+		case 'L':
+			st.push(50);
+			break;
+		case 'C':
+			st.push(100);
+			break;
+		case 'D':
+			st.push(500);
+			break;
+		case 'M':
+			st.push(1000);
+			break;
+		}
+	}
+	while (!st.empty())
+	{
+		int tmp = st.top();
+		st.pop();
+		if (st.empty())
+		{
+			result += tmp;
+			break;
+		}
+		int tmp_1 = st.top();
+		st.pop();
+		if (tmp == 5 && tmp_1 == 1)
+		{
+			result += 4;
+		}
+		else if (tmp == 10 && tmp_1 == 1)
+		{
+			result += 9;
+		}
+		else if (tmp == 50 && tmp_1 == 10)
+		{
+			result += 40;
+		}
+		else if (tmp == 100 && tmp_1 == 10)
+		{
+			result += 90;
+		}
+		else if(tmp == 500 && tmp_1 == 100)
+		{
+			result += 400;
+		}
+		else if (tmp == 1000 && tmp_1 == 100)
+		{
+			result += 900;
+		}
+		else
+		{
+			result = result + tmp + tmp_1;
+		}
+	}
+	return result;
+}
+
+int romanToInt_2(string s)
+{
+	int result = 0;
+	int pre = 0;
+	for (int i=0; i<s.size(); ++i)
+	{
+		switch (s.at(i))
+		{
+		case 'I':
+			result += 1;
+			pre = 1;
+			break;
+		case 'V':
+			if (pre == 1)
+			{
+				result += 3;
+			}
+			else
+			{
+				result += 5;
+			}
+			pre = 5;
+			break;
+		case 'X':
+			if (pre == 1)
+			{
+				result += 8;
+			}
+			else
+			{
+				result += 10;
+			}
+			pre = 10;
+			break;
+		case 'L':
+			if (pre == 10)
+			{
+				result += 30;
+			}
+			else
+			{
+				result += 50;
+			}
+			pre = 50;
+			break;
+		case 'C':
+			if (pre == 10)
+			{
+				result += 80;
+			}
+			else
+			{
+				result += 100;
+			}
+			pre = 100;
+			break;
+		case 'D':
+			if (pre == 100)
+			{
+				result += 300;
+			}
+			else
+			{
+				result += 500;
+			}
+			pre = 500;
+			break;
+		case 'M':
+			if (pre == 100)
+			{
+				result += 800;
+			}
+			else
+			{
+				result += 1000;
+			}
+			pre = 1000;
+			break;
+		}
+	}
+	return result;
 }
 
 int main()
@@ -156,6 +321,16 @@ int main()
 	std::cout << endl;
 	*/
 	//反转整数
-	int test = 120;
-	std::cout << reverseNum_1(test) << endl;
+	/*
+	int test = -123;
+	std::cout << reverseNum(test) << endl;
+	*/
+	//回文数
+	/*
+	int test = 10;
+	std::cout << isPalindrome_2(test) << endl;
+	*/
+	//罗马数到int
+	string s = "MCMXCIV";
+	std::cout << romanToInt_1(s) << endl;
 }
